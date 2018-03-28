851bc2a47e6f9c7586901d1b679e9a440ea0e1f7
Leonardo Iacovini <leoiacovini@gmail.com>
2018-03-28 17:06:43 -0300

    Add create-card test

--- test/payments/service_test.clj
+++ test/payments/service_test.clj
@@ -3,15 +3,28 @@
             [payments.service :as ser]
             [common-labsoft.test-helpers :as th]
             [matcher-combinators.midje :refer [match]]
-            [matcher-combinators.matchers :as m]
             [common-labsoft.misc :as misc]))
 
 (def card-id (misc/uuid))
 (def order-id (misc/uuid))
+(def customer-id (misc/uuid))
 
 (th/with-service [ser/start! ser/stop!] [system service]
+  (fact "Creates a new card"
+    (th/request! service :post "/api/cards" {:card {:name        "test"
+                                                    :number      "1234"
+                                                    :cvv         "123"
+                                                    :exp-date    #time/date "2020-10-10"
+                                                    :customer-id customer-id}})
+    => (match {:card {:id          string?
+                      :name        "test"
+                      :number      "1234"
+                      :cvv         "123"
+                      :exp-date    "2020-10-10"
+                      :customer-id (str customer-id)}}))
+
   (th/with-token {:token/scopes ["jaiminho"]}
-    (fact "Http Test"
+    (fact "Creates a new payment request"
       (th/request! service :post "/api/payments" {:payment {:amount   50.0M
                                                             :card-id  card-id
                                                             :order-id order-id}})
